46ed4e467f8451a7051352852d2176dd51e65c4c
Max Kobzar <nimbus2219@gmail.com>
2025-08-05 01:36:59 +0100

    dynamic main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { fetchAllImages } from '@/actions/r2-actions';
 import VerticalRandomPhoto from '@/components/VerticalImage';
 import { ROUTE_ABOUT, ROUTE_PORTFOLIO } from '@/routes/routes';
