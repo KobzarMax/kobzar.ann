e31624de12e8a0abaad87829de8d9770c28b92bc
Max Kobzar <nimbus2219@gmail.com>
2025-08-11 01:17:03 +0100

    update to merge

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,12 +5,10 @@ import PhotoPopup from '@/components/PhotoPopup';
 export default async function PortfolioPage() {
   const renderingPhotos = await getPhotos();
 
-  const reversedPhotos = [...renderingPhotos].reverse();
-
   return (
     <main className="bg-white">
-      <Gallery photos={reversedPhotos} />
-      <PhotoPopup carouselPhotos={reversedPhotos} />
+      <Gallery photos={renderingPhotos} />
+      <PhotoPopup carouselPhotos={renderingPhotos} />
     </main>
   );
 }
